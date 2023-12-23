601fccf6b4ba5342d1b2c35b42e0d7c848c803f2
Nikita Lazarev <niklz@csail.mit.edu>
2023-12-23 02:27:01 -0800

    datasets added

--- dataset/extract.sh
+++ dataset/extract.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# ./extract.sh silesia
+# Usage: ./extract.sh
 
-source_directory=$1
+#
+# Extract silesia corpus.
+#
+source_directory='silesia'
 target_directory=${source_directory}_tmp
 
+mkdir ${source_directory}
+mkdir ${target_directory}
+
+# Download silesia dataset.
 cd ${source_directory}
 wget https://sun.aei.polsl.pl//~sdeor/corpus/dickens.bz2
 wget https://sun.aei.polsl.pl//~sdeor/corpus/mozilla.bz2
@@ -20,13 +27,43 @@ wget https://sun.aei.polsl.pl//~sdeor/corpus/xml.bz2
 wget https://sun.aei.polsl.pl//~sdeor/corpus/x-ray.bz2
 cd ..
 
+# Extract.
+for file in $source_directory/*; do
+    # Check if it is a regular file
+    if [ -f "$file" ]; then
+        echo "Decompressing $file into $target_directory"
+        # Extract the filename from the path
+        filename=$(basename "$file")
+        # Decompress the file into the target directory
+        bzip2 -dkc "$file" > "$target_directory/${filename%.bz2}"
+    fi
+done
+
+#
+# Extract wiki dataset.
+#
+source_directory='wiki'
+target_directory=${source_directory}_tmp
+
+mkdir ${source_directory}
 mkdir ${target_directory}
 
-if [ ! -d "$source_directory" ]; then
-    echo "Error: Directory '$source_directory' does not exist."
-    exit 1
-fi
+# Download wiki dataset.
+cd ${source_directory}
+wget https://mattmahoney.net/dc/enwik9.zip
+cd ..
 
+# Extract.
+unzip ${source_directory}/enwik9.zip -d ${target_directory}/
+
+#
+# Extract snapshot dataset.
+#
+source_directory='snapshots'
+target_directory=${source_directory}_tmp
+mkdir ${target_directory}
+
+# Extract.
 for file in $source_directory/*; do
     # Check if it is a regular file
     if [ -f "$file" ]; then
