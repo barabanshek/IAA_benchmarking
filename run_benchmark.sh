dac874aec54eecad48e743d1a859ffb24a1d9f2b
Nikita <nikita@spr-ext-2.research.intel-research.net>
2024-03-28 17:31:43 -0700

    venv added

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -10,4 +10,5 @@ echo "Running IAA benchmarks for "${repetitions} "iterations; save results under
 sudo LD_LIBRARY_PATH=/usr/local/lib ./build/iaa_bench --benchmark_repetitions=${repetitions} --benchmark_min_time=1x --benchmark_filter=${filter} --benchmark_format=csv --logtostderr | tee results.csv
 
 # Plot results.
+source /.venv/bin/activate
 python3 plot_benchmark.py results.csv ${name}
