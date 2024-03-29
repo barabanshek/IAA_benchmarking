3dc5f6130fb7beb7cff10c0df9e900c1432002c2
Nikita Lazarev <niklz@csail.mit.edu>
2024-03-28 18:05:23 -0700

    small change

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -10,6 +10,9 @@ echo "Running IAA benchmarks for "${repetitions} "iterations; filter:" ${filter}
 # Run benchmarks.
 sudo LD_LIBRARY_PATH=/usr/local/lib ./build/iaa_bench --benchmark_repetitions=${repetitions} --benchmark_min_time=1x --benchmark_filter=${filter} --benchmark_format=csv --logtostderr | tee results.csv
 
+# Save results.
+cp results.csv out/
+
 # Plot results.
 source /.venv/bin/activate
 python3 plot_benchmark.py results.csv ${result_name}
